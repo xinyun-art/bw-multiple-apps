cb1213bc3cd8ce5fd917bb8f0f0d78c6001adeda
yfcurt <yfcurt@itcom888.com>
2024-04-05 18:32:26 +0800

    fix:233

---
+++ packages/eslint-conf/project.js
@@ -0,0 +1,23 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off'
+  },
+  globals: {
+    'process': true,
+    'require': true,
+    'module': true,
+    '__dirname': true
+  }
+}
